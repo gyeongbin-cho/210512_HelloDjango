f9c3f811ba5198a57df86c3b1c07f4e6807f8124
gyeongbin Cho <kljl1013@naver.com>
2021-05-12 20:23:49 +0900

    firstcommit

---
+++ practice/day1.py
@@ -0,0 +1,37 @@
+# 상수와변수, 입력과 출력
+# leader = "조경빈"
+# leader = input("반장의 이름을 입력하세요")
+# print("우리반 반장 이름은", leader)
+
+#문자형
+# num1 = int(input("첫번째 정수를 입력하세요 : "))
+# num2 = int(input("두번째 정수를 입력하세요 : "))
+
+# print(num1+num2)
+
+#문자형 2
+# li = [3, 1, "배가", 4, "고파요", 5, 1, 4, 4]
+# print(li[2])
+# print(li[0:2])
+# print(len(li))
+# print(li.count(4))
+# print(li.index("배가"))
+
+#리스트 원소 오름차순으로 정령해주는 함수: 변수.sort()
+# new = [1, 5, 4, 3, 2]
+# new.sort()
+# print(new)
+
+#딕셔너리
+# pairs = {'기리보이' : 'FLEX', '세븐틴' : '어쩌나'}
+# print(pairs)
+# pairs['그린데이'] = '홀리데이' (추가)
+# print(pairs)
+# del pairs['기리보이']
+# print(pairs) 삭제
+# song = pairs.get('세븐틴') - 추가
+# print(song)
+
+
+
+
---
+++ practice/day3.py
@@ -0,0 +1,22 @@
+# 실습1
+# money = int(input("돈을 입력하세요"))
+
+
+# def func(money):
+#     if(money >= 10000):
+#         print("고기를 먹는다")
+#     elif(5000 <money ):
+#         print("국밥을 먹는다")
+#     else:
+#         print("라면을 먹는다")
+
+
+# func(money)   
+
+
+fruits = ["딸기", "수박", "메론", "사과", "포도"]
+
+
+for fruits in range (1,6):
+   print(fruits,"번 과일을 입력하세요")
+
